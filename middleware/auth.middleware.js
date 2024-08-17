0fd5cf7e5b43185a08a3c68c79304c9639d94bda
mothies-m <mothieswaran711@gmail.com>
2024-08-17 09:53:46 +0530

    Update: jwt

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,2 +1,22 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
+const authenticateUser = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Token is required' });
+    }
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ error: 'Token is required' });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({ error: 'Invalid token' });
+    }
+};
+
+module.exports = authenticateUser;
