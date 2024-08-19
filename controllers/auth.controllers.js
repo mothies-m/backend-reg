1cd49f031523ba6ca21d8bbaf75507669853e2e6
mothies-m <mothieswaran711@gmail.com>
2024-08-19 23:35:55 +0530

    Update: user_id

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -100,6 +100,7 @@ const verify = async (req, res) => {
     );
 
     res.status(200).json({
+      user_id: user.user_id,
       email: user.email,
       first_name: user.first_name,
       last_name: user.last_name,
